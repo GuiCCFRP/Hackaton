a4b69740bdc3beadeceff720f3fa489cbe3927e5
GuiCCFRP <monkedrip10@gmail.com>
2025-11-23 11:01:45 +0000

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -10,4 +10,5 @@ class User(UserMixin, db.Model):
     email    = db.Column(db.String(67), unique=True, nullable=False)
     password = db.Column(db.String(67), nullable=False)
 
-API_KEY = "sk-hc-v1-a116620997e2443a9f23bef82a50ec014fe9d189a8dc46339bc6f3415bb8c515"
+
+API_KEY = "sk-hc-v1-a116620997e2443a9f23bef82a50ec014fe9d189a8dc46339bc6f3415bb8c515" #YES TS IS RAGEBAIT :)))))))))
